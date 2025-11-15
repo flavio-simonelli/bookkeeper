e37d3f68319c4bc6df5a77551c2e14a4dcaa620f
Flavio <flavio.simonelli.se@gmail.com>
2025-11-15 14:21:52 +0100

    Add Jacoco analysis in Journal class

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/ExampleJournalTest.java
@@ -0,0 +1,11 @@
+package org.apache.bookkeeper.bookie;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class ExampleJournalTest {
+    @Test
+    public void testExampleJournal() throws Exception {
+        assertTrue(true);
+    }
+}
